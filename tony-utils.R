27a9c3a10c2d32ae2325554ed8cf62a37b07577d
Tony Fischetti <tony.fischetti@gmail.com>
2021-04-14 10:41:18 -0400

    now that equivalent utility functions are in libbib...
    remove from tony-utils.R

--- tony-utils.R
+++ tony-utils.R
@@ -33,48 +33,6 @@
   #return(sprintf("%.2f <-> %.2f", dascis[[type]][4], dascis[[type]][5]))
 }
 
-.env$delcols <- function(DT, ...){
-  cols <- c(...)
-  DT[, (cols):=NULL]
-}
-
-.env$keepcols <- function(DT, ...){
-  cols <- c(...)
-  these <- setdiff(names(DT), cols)
-  delcols(DT, these)
-}
-
-pivot <- function(DT, theby, theexp, cutoff=0){
-  inexp <- substitute(theexp)
-  strexp <- as.character(deparse(inexp))
-  template <- sprintf("DT[, .(val=%s), theby][order(-val)]", strexp)
-  tmp <- eval(parse(text=template))
-  thetotal <- tmp[, sum(val)]
-  tmp[, percent:=round(val/thetotal*100, 2)]
-  tmp[percent<=cutoff, (theby):="OTHER"]
-  tmp <- tmp[, .(val=sum(val), percent=sum(percent)), theby]
-  tmp2 <- tmp[1,]
-  tmp2[1,1] <- "TOTAL"
-  tmp2[1,2] <- thetotal
-  tmp2[1,3] <- 100
-  rbind(tmp, tmp2)
-}
-
-cleannames <- function(dat){
-  thenames <- names(dat)
-  nospace <- stringr::str_replace_all(thenames, "\\s+", "_")
-  nobad <- stringr::str_replace_all(nospace, "[^_A-Za-z1-9]", "_")
-  return(nobad)
-}
-
-setcleannames <- function(dat){
-  setnames(dat, cleannames(dat))
-}
-
-percent.not.na <- function(aframe, avar){
-  aframe[, round(100*sum(!is.na(get(avar)))/.N, 2)]
-}
-
 
 attach(.env)
 
