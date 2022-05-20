1a009837e6603bd762647707a5aae961707cc021
Tony Fischetti <tony.fischetti@gmail.com>
2022-05-20 08:16:18 -0400

    added more to install script

--- install.sh
+++ install.sh
@@ -4,3 +4,6 @@ ln -s ~/.rix/Rprofile ~/.Rprofile
 ln -s ~/.rix/inputrc ~/.inputrc
 ln -s ~/.rix/Renviron ~/.Renviron
 mkdir -p ~/local/R_libs
+
+R -e 'install.packages(c("devtools", "assertr", "libbib", "data.table"))'
+R -e 'devtools::install_github("jalvesaq/colorout")'
