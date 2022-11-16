4f496a7dfb7adc62287bc40186ecd764afc235f7
Tony Fischetti <tony.fischetti@gmail.com>
2022-11-16 13:34:52 -0500

    whoops

--- install.sh
+++ install.sh
@@ -6,4 +6,4 @@ ln -s ~/.rix/Renviron ~/.Renviron
 mkdir -p ~/local/R_libs
 
 R -e 'install.packages(c("assertr", "libbib", "data.table", "ggplot2"))'
-R CMD install goodies/colorout_1.2-1.tar.gz
+R CMD INSTALL goodies/colorout_1.2-1.tar.gz
