00bc86cba24f42d1e9c2e1f5f4e574af9193b743
Tony Fischetti <tony.fischetti@gmail.com>
2022-11-16 13:41:51 -0500

    colorout didn't install?

--- install.sh
+++ install.sh
@@ -5,5 +5,5 @@ ln -s ~/.rix/inputrc ~/.inputrc
 ln -s ~/.rix/Renviron ~/.Renviron
 mkdir -p ~/local/R_libs
 
-R -e 'install.packages(c("assertr", "libbib", "data.table", "ggplot2"))'
-R CMD INSTALL goodies/colorout_1.2-1.tar.gz
+R_LIBS=~/local/R_libs/ R -e 'install.packages(c("assertr", "libbib", "data.table", "ggplot2"))'
+R_LIBS=~/local/R_libs/ R CMD INSTALL goodies/colorout_1.2-1.tar.gz
