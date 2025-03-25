dcdfa1f0c2590a2d793592ce3a4743a14222e934
Tony Fischetti <tony.fischetti@gmail.com>
2025-03-25 12:55:35 -0400

    added new packages to autoinstall

--- install.sh
+++ install.sh
@@ -7,5 +7,5 @@ ln -s ~/.rix/inputrc ~/.inputrc
 ln -s ~/.rix/Renviron ~/.Renviron
 mkdir -p ~/local/R_libs
 
-R -e 'install.packages(c("assertr", "libbib", "data.table", "ggplot2"))'
+R -e 'install.packages(c("assertr", "libbib", "data.table", "ggplot2", "tidyverse", "languageserver"))'
 R CMD INSTALL goodies/colorout_1.2-1.tar.gz
